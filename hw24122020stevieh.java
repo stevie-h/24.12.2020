6194b21609f284e27fa1f36bcbe06f86ec0f71c9
Stevie H <74920914+stevie-h@users.noreply.github.com>
2020-12-27 18:47:42 +0200

    Create hw24122020stevieh.java

---
+++ hw24122020stevieh.java
@@ -0,0 +1,42 @@
+/* HW from 24.12.2020
+-- 1. create an array of double numbers with these numbers: 0.9, -6.5555, 3.24, 10.11, 20.35
+-- 1.1. print the first element
+-- 1.2. print the last element
+-- 1.3. print the middle element
+-- 1.4. write a for loop which prints all the element in the array
+-- 1.5. write a for loop which prints all the element in the array in reverse
+-- 1.6. use scanner to read a double number from the user. print all the elements in the array which are bigger than the user-number
+-- 2. create an array A of integer numbers with these numbers: 4, 10, 20, 40, 1000
+-- 2.1. create a second array B with these numbers: 0, 0, 0, 0, 0
+-- 2.2. copy all elements from A to B
+-- ***crazy etgar: 
+-- create an array of numbers: 1, 3, 5, 8, 8, 5, 3, 1
+-- check if the array is a polindrom (which in this case it is)
+-- polindrom = all elements are repeated in a reverse order:
+-- here it is polidrom since --> 1 3 5 8 are reverse equal to 8 5 3 1
+-- another polindrom: 3 4 6 9 9 6 4 3
+-- not polindrom: 1 3 5 6 5 3 1 --> since 6 is not repeated
+
+*/
+
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner prn = new Scanner(System.in);
+        Random r = new Random();
+        
+        
+        
+        
+        
+
+
+    }
+}
